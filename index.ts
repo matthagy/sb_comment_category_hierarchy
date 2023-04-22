e18b25a37b3d55ae1da6a0e89e8f61067eb1bd65
Matt Hagy <matthew.hagy@gmail.com>
2023-04-22 15:33:28 -0400

    Improved visibility toggling after learning more about D3

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ class Node {
     count: number;
     children: Node[];
     visible: boolean = true;
+    childCollapses: boolean = false;
 
     constructor(id: string, title: string, count: number, children: Node[]) {
         this.id = id;
@@ -20,14 +21,15 @@ class Node {
         return this.children;//.filter(c => c.visible);
     }
 
-    toggleVisible(): void {
-        this.visible = !this.visible;
-    }
-
-    toggleChildrenVisible(): void {
+    toggleChildrenVisible(collapse?: boolean): void {
+        if (collapse === undefined) {
+            collapse = !this.childCollapses;
+            // console.log(`toggleChildrenVisible ${this.title} collapse=${collapse} visible=${this.visible}`);
+        }
+        this.childCollapses = collapse;
         for (const child of this.children) {
-            child.toggleVisible();
-            child.toggleChildrenVisible();
+            child.visible = !collapse;
+            child.toggleChildrenVisible(collapse);
         }
     }
 }
@@ -64,65 +66,145 @@ const treeLayout = d3.tree<Node>()
 let rootNode = d3.hierarchy(topLevelNode, d => d.getChildren())
 let layoutNode = treeLayout(rootNode);
 
-const links = g.selectAll(".link")
-    .data(layoutNode.descendants().slice(1))
-    .enter()
-    .append("path")
-    .style("stroke", "#ccc")
-    .attr("fill", "none")
-    .attr("r", 3)
-    .attr("class", "link")
-    .attr("d", d => {
-        const parent = d.parent;
-        if (!parent) {
-            throw new Error("Parent is null");
-        }
-        return "M" + d.y + "," + d.x
-            + "C" + (d.y + parent.y) / 2 + "," + d.x
-            + " " + (d.y + parent.y) / 2 + "," + parent.x
-            + " " + parent.y + "," + parent.x;
-    });
+type NodeSelectionType = d3.Selection<BaseType, d3.HierarchyPointNode<Node>, SVGGElement, unknown>;
+type LinkPathType = d3.Selection<SVGPathElement, d3.HierarchyPointNode<Node>, SVGGElement, unknown>
+type NodePathType = d3.Selection<SVGGElement, d3.HierarchyPointNode<Node>, SVGGElement, unknown>
+type NodeShapeType = d3.Selection<SVGCircleElement, d3.HierarchyPointNode<Node>, SVGGElement, unknown>
+type NodeTextType = d3.Selection<SVGTextElement, d3.HierarchyPointNode<Node>, SVGGElement, unknown>
 
-const nodes = g
-    .selectAll(".node")
-    .data(layoutNode.descendants())
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
+function selectLinks(): NodeSelectionType {
+    return g.selectAll(".link")
+        .data(layoutNode.descendants().slice(1));
+}
 
-const nodeCircles = nodes
-    .append("circle")
-    .attr("r", 10)
-    .attr("fill", "steelblue")
-    .on("click", (event, d) => {
-        console.log(`Clicked ${d.data.title} ${d.data.visible}}`);
-        d.data.toggleChildrenVisible();
-        update();
-    });
+function selectNodes(): NodeSelectionType {
+    return g
+        .selectAll(".node")
+        .data(layoutNode.descendants());
+}
 
-const nodeTexts = nodes
-    .append("text")
-    .text((d) => `${d.data.title} (${d.data.count})`)
-    .attr("transform", function (d) {
-        return "translate(12,5)";
-    });
+function linkAppend(selection: NodeSelectionType): LinkPathType {
+    return selection
+        .enter()
+        .append("path");
+}
+
+function nodeAppend(selection: NodeSelectionType): NodePathType {
+    return selection
+        .enter()
+        .append("g");
+}
+
+function linkStyle(selection: LinkPathType): LinkPathType {
+    return selection
+        .style("stroke", "#ccc")
+        .style("stroke-width", 3)
+        .attr("fill", "none")
+        //.attr("r", 3)
+        .attr("class", "link")
+        .attr("opacity", d => d.data.visible ? 0.25 : 0)
+        .attr("d", d => {
+            const parent = d.parent;
+            if (!parent) {
+                throw new Error("Parent is null");
+            }
+            return "M" + d.y + "," + d.x
+                + "C" + (d.y + parent.y) / 2 + "," + d.x
+                + " " + (d.y + parent.y) / 2 + "," + parent.x
+                + " " + parent.y + "," + parent.x;
+        });
+}
+
+function nodeStyle(selection: NodePathType): NodePathType {
+    return selection
+        .attr("class", "node")
+        .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
+}
+
+function nodeShapeAppend(selection: NodePathType): NodeShapeType {
+    return selection
+        .append("circle");
+}
+
+function nodeRadius(d: d3.HierarchyPointNode<Node>) {
+    return 2.3 * Math.sqrt(d.data.count);
+}
+
+function nodeShapeStyle(selection: NodeShapeType): NodeShapeType {
+    return selection
+        .attr("r", d => nodeRadius(d))
+        .attr("fill", d => d.data.children.length > 0 ? "steelblue" : "orange")
+        .attr("opacity", d => d.data.visible ? 1 : 0)
+        .on("click", (e, d) => {
+            if (e.ctrlKey) {
+                console.log(`Clicked w/ Control ${d.data.title} ${d.data.visible}}`);
+                d.data.toggleChildrenVisible();
+                update();
+            } else {
+                console.log(`Bare clicked ${d.data.title} ${d.data.visible}}`);
+            }
+        });
+}
+
+function nodeTextAppend(selection: NodePathType): NodeTextType {
+    return selection
+        .append("text");
+}
+
+function nodeTextStyle(selection: NodeTextType): NodeTextType {
+    return selection
+        .text((d) => `${d.data.title} (${d.data.count})`)
+        .attr("transform", d => `translate(${1.2 * nodeRadius(d)},5)`)
+        .attr("opacity", d => d.data.visible ? 1 : 0);
+}
+
+
+let existingLink = linkStyle(linkAppend(selectLinks()));
+let existingNode = nodeStyle(nodeAppend(selectNodes()));
+let existingNodeShape = nodeShapeStyle(nodeShapeAppend(existingNode));
+let existingNodeText = nodeTextStyle(nodeTextAppend(existingNode));
 
 function update() {
     console.log("update");
 
     rootNode = d3.hierarchy(topLevelNode, d => d.getChildren());
     layoutNode = treeLayout(rootNode);
-    links.data(layoutNode.descendants().slice(1));
-    nodes.data(layoutNode.descendants());
-    links.attr("opacity", d => d.data.visible ? 0.25 : 0);
-    nodeCircles.attr("opacity", d => d.data.visible ? 1 : 0);
-    nodeTexts.attr("opacity", d => d.data.visible ? 1 : 0);
 
+    const newLinks = linkAppend(selectLinks());
+    const mergeLinks = linkStyle(newLinks.merge(existingLink));
+    mergeLinks.exit().remove();
+    existingLink = mergeLinks;
+
+    const newNode = nodeAppend(selectNodes());
+    const mergeNode = nodeStyle(newNode.merge(existingNode));
+    mergeNode.exit().remove();
+    existingNode = mergeNode;
+
+    const newNodeShape = nodeShapeAppend(newNode);
+    const mergeNodeShape = nodeShapeStyle(newNodeShape.merge(existingNodeShape));
+    mergeNodeShape.exit().remove();
+    existingNodeShape = mergeNodeShape;
+
+    const newNodeText = nodeTextAppend(newNode);
+    const mergeNodeText = nodeTextStyle(newNodeText.merge(existingNodeText));
+    mergeNodeText.exit().remove();
+    existingNodeText = mergeNodeText;
 }
 
+function setInitialVisibility(node: Node, depth: number = 0) {
+    if (depth >= 3) {
+        node.toggleChildrenVisible();
+    } else {
+        node.children.forEach(child => setInitialVisibility(child, depth + 1));
+    }
+}
+
+setInitialVisibility(topLevelNode);
 update();
 
+function castPoint(x: any): d3.HierarchyPointNode<Node> {
+    return x as d3.HierarchyPointNode<Node>;
+}
 
 // Add zoom behavior to the svg element
 const zoom = d3
@@ -132,9 +214,9 @@ const zoom = d3
     .on("zoom", e => {
         // console.log(`Zoom: ${e.transform}`);
         g.attr("transform", e.transform);
-        g.style("stroke-width", 3 / Math.sqrt(e.transform.k));
-        g.selectAll(".node").attr("r", 5 / Math.sqrt(e.transform.k));
-        g.selectAll(".like").attr("r", 3 / Math.sqrt(e.transform.k));
+        //g.style("stroke-width", 3 / Math.sqrt(e.transform.k));
+        //g.selectAll(".node").attr("r", d => Math.sqrt(castPoint(d).data.count) / Math.sqrt(e.transform.k));
+        //g.selectAll(".like").attr("r", 3 / Math.sqrt(e.transform.k));
     });
 svg.call(zoom);
 
